b78054af5344200688aab162706e98364945f8fe
Saroja-N <pvnlsaroja@gmail.com>
2019-01-06 12:16:55 -0800

    Imports cleanup

--- src/main/java/com/myjavaassignments/JSONProcessingUtils.java
+++ src/main/java/com/myjavaassignments/JSONProcessingUtils.java
@@ -4,7 +4,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
